3249320fa097f33d9669ff6b8629d35d04544c11
runticle <runticle@gmail.com>
2023-03-12 19:19:58 +0000

    Wallet displays address, balance and private key

--- app/wallet/index.tsx
+++ app/wallet/index.tsx
@@ -1,28 +1,40 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 
 import styled from 'styled-components/native';
 import { Container } from '../../components/Globals';
 
-import { Stack, useRouter } from 'expo-router';
+import { Stack } from 'expo-router';
 import { useMnemonic } from '../../context/MnemonicContext';
-import NormalText from '../../components/Text/NormalText';
+import { getSecuredItem } from '../../utils/secureStoreUtils';
+import Revealer from '../../components/Revealer';
+import { bigNumInEth, getBalanceFromWallet } from '../../utils/ethersUtils';
 
 const WalletView = styled(Container)`
-  justify-content: space-between;
   align-items: center;
 `
 
 const WalletScreen: FunctionComponent = () => {
   const { state } = useMnemonic()
 
+  const [privateKey, setPrivateKey] = useState('')
+  const [balance, setBalance] = useState('')
+
   const { wallet } = state;
+
+  useEffect(() => {
+    if(wallet?.address) {
+      getSecuredItem(wallet?.address).then((value) => setPrivateKey(value))
+      getBalanceFromWallet(wallet.address).then((balance) => setBalance(bigNumInEth(balance)))
+    }
+
+  }, [wallet.address])
 
   return (  
     <WalletView>
-      <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} />
-      <NormalText>
-        {wallet?.address}
-      </NormalText>
+      <Stack.Screen options={{ title: 'Wallet', gestureEnabled: false }} />
+      <Revealer title="Address" textToHide={wallet?.address} />
+      <Revealer title="Balance" textToHide={balance} />
+      <Revealer title="Private Key" textToHide={privateKey} />
     </WalletView>
   );
 }
