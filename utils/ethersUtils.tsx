1aba4fd84dfc87843e445cf7bb3096ddaad877b9
runticle <runticle@gmail.com>
2023-03-12 19:19:29 +0000

    ethers util for setting up provider in dev mode

--- utils/ethersUtils.tsx
+++ utils/ethersUtils.tsx
@@ -1,7 +1,7 @@
 import "@ethersproject/shims";
 import { BigNumber, ethers, Wallet } from 'ethers';
 
-import { ETHERSCAN_API_KEY } from "@env";
+import { ETHERSCAN_API_KEY, GANACHE_NETWORK } from "@env";
 
 function getWalletFromMnemonic(phrase: string) : Wallet {
     const wallet = Wallet.fromMnemonic(phrase)
@@ -17,11 +17,24 @@ async function getBalanceFromWallet(address: string) : Promise<BigNumber> {
     return balance;
 }
 
-function setupProvider() { 
-    return new ethers.providers.EtherscanProvider('homestead', ETHERSCAN_API_KEY)
+type ProviderType = ethers.providers.BaseProvider;
+
+function setupProvider() : ProviderType { 
+    if(process.env.NODE_ENV === 'development') {
+        // * Ganache Chain
+        console.log("😇 We are using DEV mode. Make sure you have Ganache running...")
+        return ethers.providers.getDefaultProvider(GANACHE_NETWORK);
+    } else {
+        return new ethers.providers.EtherscanProvider('homestead', ETHERSCAN_API_KEY)
+    }
+}
+
+function bigNumInEth(bn: BigNumber) : string {
+    return ethers.utils.formatEther(bn)
 }
 
 export {
     getWalletFromMnemonic,
     getBalanceFromWallet,
+    bigNumInEth,
 }
