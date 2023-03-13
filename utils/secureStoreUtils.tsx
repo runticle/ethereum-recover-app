065bdcc62afd7b2f401e3f4e5f2ce686f79b75af
runticle <runticle@gmail.com>
2023-03-13 11:20:28 +0000

    lint

--- utils/secureStoreUtils.tsx
+++ utils/secureStoreUtils.tsx
@@ -2,31 +2,27 @@ import * as SecureStore from 'expo-secure-store';
 
 // TODO Create a SecureStore reducer and handle errors
 
-async function saveSecurely(key: string, value: string) : Promise<void> {
-    await SecureStore.setItemAsync(key, value)
-    console.log(key + " saved in storage")
+async function saveSecurely(key: string, value: string): Promise<void> {
+    await SecureStore.setItemAsync(key, value);
+    console.log(key + ' saved in storage');
 }
 
-async function getSecuredItem(key: string) : Promise<string> {
-    let result = await SecureStore.getItemAsync(key);
+async function getSecuredItem(key: string): Promise<string> {
+    const result = await SecureStore.getItemAsync(key);
     if (result) {
-        return result
+        return result;
     } else {
         throw new Error('We could not retrieve from Secure Storage.');
     }
 }
 
-async function deleteSecuredItem(key: string) : Promise<void> {
+async function deleteSecuredItem(key: string): Promise<void> {
     try {
-        await SecureStore.deleteItemAsync(key)
-        console.log("Deleted " + key + " from secure store")
+        await SecureStore.deleteItemAsync(key);
+        console.log('Deleted ' + key + ' from secure store');
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
-export {
-    saveSecurely,
-    getSecuredItem,
-    deleteSecuredItem,
-}
+export { saveSecurely, getSecuredItem, deleteSecuredItem };
